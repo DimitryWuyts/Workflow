5c79d2303f76a489221a2cc4cc60c68101a1b926
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-03-10 16:05:32 +0100

    It works!

--- src/Controller/CustController.php
+++ src/Controller/CustController.php
@@ -2,13 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
+use App\Form\TicketFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 /**
- * @Route("/cust")
+ * @Route("/cust", name ="cust_View")
  */
 class CustController extends AbstractController
 {
+    /**
+     * @Route("/create", name ="cust_createTicket")
+     */
+    public function createTicket(Request $request)
+    {
+        $ticket = new Ticket();
+        $form = $this->createForm(TicketFormType::class, $ticket);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ticket->setTicketTitle($form->get('ticketTitle')->getData());
+            $ticket->setStatus($form->get('status')->getData());
+            $ticket->setDate($form->get('date')->getData());
+            $ticket->setContent($form->get('content')->getData());
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($ticket);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+
+
+
+
+        }
+        return $this->render('cust/ticketView.html.twig', [
+            'ticketForm' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/", name="cust_index")
      */
