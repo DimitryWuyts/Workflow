1cdebf8029f1ee5395141db5c7b50e304760849c
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-03-10 16:24:14 +0100

    Repushing

---
+++ src/Form/TicketFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ticketTitle')
+            ->add('status')
+            ->add('level')
+            ->add('date')
+            ->add('content')
+            ->add('counter')
+            ->add('prioritylevel')
+            ->add('users')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
